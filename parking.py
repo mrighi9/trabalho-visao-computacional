85d21882dc6b1b6754c5af4d0818ab3d9dcbac55
rzanon1 <rafaelzanoni2018@gmail.com>
2025-10-30 20:08:48 -0300

    Primeiro commit

---
+++ parking.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+import pickle
+
+
+
+def parking():
+   
+
+    video_path = ""
+
+
+    cap = cv2.VideoCapture(video_path)
+    while True:
+
+    
+        
+        cv2.imshow("Identificador/Contador de vagas")
+        
+
+
+if __name__ == "__main__":
+    parking()
