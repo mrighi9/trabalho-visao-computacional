8e50d978499d66a35196d63ec6cf58e5c5934fd7
rzanon1 <rafaelzanoni2018@gmail.com>
2025-11-12 20:05:48 -0300

    gerador final

--- gerador_coordenada_estacionamento.py
+++ gerador_coordenada_estacionamento.py
@@ -10,3 +10,27 @@ def demostration():
 
     image_path = "src/exemplo.png"
     rect_width, rect_height = coordinate_generator.rect_width, coordinate_generator.rect_height
+    
+    while True:
+        
+        image =cv2.imread(image_path)
+
+        for pos in coordinate_generator.posicao_carro_vaga: 
+            
+            start = pos
+            end = (pos[0]+rect_width, pos[1]+rect_height)
+
+            cv2.rectangle(image,start,end,(0,0,255),2)
+        
+        cv2.imshow("Image",image)
+
+        cv2.setMouseCallback("Image",coordinate_generator.mouseClick)
+
+        
+        if cv2.waitKey(1) == ord("q"):
+            break
+
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    demostration()
